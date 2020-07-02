bdd00d970917f58543f9d739de53e1551088d5b5
MBurnette4120 <mikeburnette@rocketmail.com>
2020-07-02 14:05:44 -0400

    Add project files.

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    public class Program     
+
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Fahrenheit temperature:");
+            double newTempC = double.Parse(Console.ReadLine());
+            double theTemp = TempConverter.FahrenheitToCelsius(newTempC);            
+            Console.WriteLine($"{theTemp} degrees Celsius");
+
+
+            Console.WriteLine("Enter the Celsius temperature:");
+            double newTempF = double.Parse(Console.ReadLine());
+            double newTemp = TempConverter.CelsiusToFahrenheit(newTempF);
+            Console.WriteLine($"{newTemp} degrees Fahrenheit");
+
+
+        }
+    }
+
+    
+
+
+}
+
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+       public static double FahrenheitToCelsius(double tempF)
+        {
+            double tempCelsius = (tempF - 32) * 5 / 9;
+                return tempCelsius;
+            //(32°F − 32) × 5/9 = 0°C
+
+
+        }
+
+        public static double CelsiusToFahrenheit(double tempC)
+        {
+            double tempFahrenheit = (tempC *  9/5) + 32;
+            return tempFahrenheit;
+
+            //(0°C × 9 / 5) +32 = 32°F
+
+        }
+    }
+
+    
+
+
+
+
+
+
+
+}
